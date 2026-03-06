1ac24cbcb689498de38474fcc7531aa6e9cd0e23
Alejandro4092 <acobomarcos@gmail.com>
2026-03-06 14:06:41 +0100

    Add project files.

---
+++ MvcNetCorePaginacionRegistros/Controllers/PaginacionController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCorePaginacionRegistros.Models;
+using MvcNetCorePaginacionRegistros.Repositories;
+
+namespace MvcNetCorePaginacionRegistros.Controllers
+{
+    public class PaginacionController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public PaginacionController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+        public async Task<IActionResult> RegistroVistaDepartamento(int? posicion)
+        {
+            if(posicion == null)
+            {
+                posicion = 1;
+            }
+            int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            int siguiente = posicion.Value + 1;
+            if (siguiente > numRegistros)
+            {
+                siguiente = numRegistros;
+            }
+            int anterior = posicion.Value - 1;
+            if (anterior < 1)
+            {
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = numRegistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            VistaDepartamento vista = await this.repo.GetVistaDepartamentoAsync(posicion.Value);
+            return View(vista);
+        }
+
+        public async Task<IActionResult> GrupoVistaDepartamentos(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            // LOS SIGUIENTE SERA QUE DEBERMOS DIBUJAR LOS NUMEROS DE PAGINA EN LOS LINKS
+            // NECESITAMOS UNA VARIABLE PARA EL NUMERO DE PAGINA
+            // VOY A REALIZAR EL DIBUJO DESDE AQUI, NO DESDE RAZOR
+            int numPagina = 1;
+            int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            ViewData["NUMREGISTROS"] = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            ViewData["NUMPAGINA"] = 1;
+            string html = "<div class='container'>";
+            for (int i = 1; i <= numRegistros; i+=2)
+            {
+                html += "<a href='GrupoVistaDepartamentos?posicion="+i+"'>Página "+numPagina+"</a>";
+                numPagina += 1;
+            }
+
+            html += "</div>";
+            ViewData["LINKS"] = html;
+            List<VistaDepartamento> departamentos = 
+                await this.repo.GetGrupoVistaDepartamentoAsync(posicion.Value);
+            return View(departamentos);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcNetCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcNetCorePaginacionRegistros.Data;
+using MvcNetCorePaginacionRegistros.Models;
+using System.Collections.Generic;
+
+namespace MvcNetCorePaginacionRegistros.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+        public async Task<int> GetNumeroRegistrosVistaDepartamentosAsync()
+        {
+            return await this.context.VistaDepartamentos.CountAsync();
+        }
+        public async Task<VistaDepartamento> GetVistaDepartamentoAsync(int posicion)
+        {
+            VistaDepartamento departamento =
+                await this.context.VistaDepartamentos.Where(z => z.Posicion == posicion)
+                .FirstOrDefaultAsync();
+            return departamento;
+        }
+        public async Task<List<VistaDepartamento>> GetGrupoVistaDepartamentoAsync(int posicion)
+        {
+            var consulta = from datos in this.context.VistaDepartamentos
+                           where datos.Posicion >= posicion && datos.Posicion < (posicion + 2)
+                           select datos;
+            List<VistaDepartamento> departamentos = await consulta.ToListAsync();
+            return departamentos;
+        }
+    }
+}
